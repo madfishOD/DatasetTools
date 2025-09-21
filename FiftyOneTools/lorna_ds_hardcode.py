78040edc004d8e65ddc957eb88546f85aab27518
oleg.shevchenko <oleg.shevchenko@hit.zone>
2025-09-21 14:32:03 +0300

    Losing samples of dataset fix by calling ds.compute_metadata()

--- FiftyOneTools/lorna_ds_hardcode.py
+++ FiftyOneTools/lorna_ds_hardcode.py
@@ -16,10 +16,6 @@ os.environ["FIFTYONE_DATABASE_DIR"] = DB_DIR  # root .fiftyone folder, not ...\v
 
 import fiftyone as fo
 
-# --- Vars ---
-# dataset: fo.Dataset
-# session: fo.Session
-
 # ---------- Console helpers ----------
 def stamp(msg: str) -> None:
     print(f"[{time.strftime('%H:%M:%S')}] {msg}")
@@ -64,6 +60,7 @@ def create_dataset_from_dir(root: str) -> fo.Dataset:
     paths = collect_image_paths(root)
     ds.add_images(paths)   
     ds.persistent = True
+    ds.compute_metadata()
     ds.save()
     return ds
 
@@ -80,52 +77,26 @@ def load_or_create_dataset(name: str, root: str) -> fo.Dataset:
     else:
         sample_count = ds.count()
         images_count = count_images(DATASET_DIR)
-        import_images = ask_yn(
-                        f"\nDataset '{DATASET_NAME}' already exists in DB -> {DB_DIR}\n"
-                        f"Dataset samples:{sample_count} images in dir:{images_count}\n"
-                        f"Reimport samples from {DATASET_DIR}?",
-                        False
-                        )
-        if import_images:
-            paths = collect_image_paths(root)
-            ds.add_images(paths)
-            ds.save()
-            print(f"Dataset samples imported: {ds.count()}")
+        if(sample_count != images_count):
+            import_images = ask_yn(
+                            f"\nDataset '{DATASET_NAME}' already exists in DB -> {DB_DIR}\n"
+                            f"Dataset samples:{sample_count} images in dir:{images_count}\n"
+                            f"Reimport samples from {DATASET_DIR}?",
+                            False
+                            )
+            if import_images:
+                paths = collect_image_paths(root)
+                ds.add_images(paths)
+                ds.save()
+                print(f"Dataset samples imported: {ds.count()}")
     return ds
 
 def main():
 
-    dataset_exists = fo.dataset_exists(DATASET_NAME)
-    print(f"dataset_exists = {dataset_exists}")
+    dataset = load_or_create_dataset(DATASET_NAME, DATASET_DIR)
 
-    image_paths = collect_image_paths(DATASET_DIR)
-    if(len(image_paths)>0):
-        new_samples = []
-        for p in collect_image_paths(DATASET_DIR):
-            new_samples.append(fo.Sample(filepath=p))
-
-    if(dataset_exists):
-        dataset = fo.load_dataset(DATASET_NAME)
-        print(f"Dataset '{DATASET_NAME}' loaded from DB:{DB_DIR}"
-              f"\nSamples = {dataset.count()}")
-
-        if(dataset.count()<1 and len(new_samples)>0):
-            dataset.add_samples(new_samples)
-            dataset.save()
-            print(f"Samples added: {dataset.count()}")
-
-        else:
-            print(f"Existing dataset {dataset}")
-    else:
-        dataset = fo.Dataset(name=DATASET_NAME, persistent=True)
-        dataset.media_type = "image"
-        if(len(new_samples)>0):
-            dataset.add_samples(new_samples)
-            dataset.compute_metadata()
-            dataset.save()
-
-    # session = fo.launch_app(dataset)
-    # session.wait()
+    session = fo.launch_app(dataset)
+    session.wait()
 
 if __name__ == "__main__":
     main()
