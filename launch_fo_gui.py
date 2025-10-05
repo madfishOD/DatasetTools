6acb4e0552efa4758ea344e88af23c977bdaa325
madfishOD <madfish.od@gmail.com>
2025-10-05 16:53:01 +0300

    loading mad-io plugin added

--- launch_fo_gui.py
+++ launch_fo_gui.py
@@ -1,6 +1,26 @@
 import fiftyone as fo
+import fiftyone.plugins as fopl
+import fiftyone.plugins.utils as fopu
+
+
+plugin_name = "hello_python"
+
+def setup_my_plugins() -> bool:
+    try:
+        fopl.download_plugin(
+                "https://github.com/madfishOD/FiftyOnePlugins.git",
+                plugin_names= "@madfish/hello_python",
+                overwrite=True
+            )
+        fopl.enable_plugin("@madfish/hello_python")
+    except Exception as e:
+        print(f"❌ Failed to install plugin @madfish/hello_python: {e}")
 
 def main():
+    fopl.enable_plugin("@madfish/mad-io")
+    plugins_list = fopl.list_enabled_plugins()
+    print(plugins_list)
+
     session = fo.launch_app()
     session.wait()
 
